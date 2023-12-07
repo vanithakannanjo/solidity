4f5eb18b5f43231ce5a68baeaa0aef87a48c7444
Vanitha Kannan <vanithakannanjo@gmail.com>
2023-12-07 15:41:41 +0800

    twitter contract

--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -2,14 +2,53 @@
 pragma solidity ^0.8.18;
 
 contract Twitter{
-    mapping(address => string) public tweets;
+
+    uint16 public MAX_TWEET_LENGTH=280;
+
+    struct Tweet{
+        address author;
+        string content;
+        uint256 timestamp;
+        uint256 likes;
+    }
+
+    mapping(address => Tweet[]) public tweets;
+    address public owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender == owner,"You are not the Owner");
+        _;
+    }
+
+    function changeTweetLength(uint16 newTweetLength)public onlyOwner{
+        MAX_TWEET_LENGTH =newTweetLength;
+    }
 
     function createTweet(string memory _tweet) public {
-        tweets[msg.sender] = _tweet;
+
+        //if tweet length <= 280 chars then good, otherwise revert
+        require(bytes(_tweet).length <= MAX_TWEET_LENGTH,"Tweet is too long");
+
+        Tweet memory newTweet = Tweet({
+            author: msg.sender,
+            content: _tweet,
+            timestamp:block.timestamp,
+            likes:0
+        });
+        tweets[msg.sender].push(newTweet);
     }
 
 
-    function getTweet(address _owner)public view returns(string memory){
-        return tweets[_owner]
+    function getTweet( uint _i)public view returns(Tweet memory){
+        return tweets[msg.sender][_i];
     }
+
+    function getAllTweets(address _owner)public view returns(Tweet[] memory){
+        return tweets[_owner];
+    }
+
 }
